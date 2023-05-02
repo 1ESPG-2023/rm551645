337b542694c124ac1c83b168bd3d5de7aecbf28e
bdancs <rm551645@fiap.com.br>
2023-05-02 11:52:30 -0300

    exercícios de setTimeout + input e label

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -80,30 +80,103 @@
 // soma();
 //______________________________________________________________________________25/04/2023
 
-let tmp = ""
+// let tmp = ""
 
-function mudaCor(){
+// function mudaCor(){
 
-    let r = "";
-    let g = "";
-    let b = "";
+//     let r = "";
+//     let g = "";
+//     let b = "";
 
-    //random = gera números aleatórios entre 0 e 1.
-    //ceil, floor e round = arredondam o número para cima, para baixo ou aleatoriamente.
+//     //random = gera números aleatórios entre 0 e 1.
+//     //ceil, floor e round = arredondam o número para cima, para baixo ou aleatoriamente.
 
-    //Determinando para a variável r um valor entre 0 e 255
-    r = Math.round(Math.random() * 255);
-    g = Math.round(Math.random() * 255);
-    b = Math.round(Math.random() * 255);
+//     //Determinando para a variável r um valor entre 0 e 255
+//     r = Math.round(Math.random() * 255);
+//     g = Math.round(Math.random() * 255);
+//     b = Math.round(Math.random() * 255);
 
-    //querySelector = seleciona um elemento igual o css
-    const cabeçalho = document.querySelector(".cabeçalho")
+//     //querySelector = seleciona um elemento igual o css
+//     const cabeçalho = document.querySelector(".cabeçalho")
 
-    cabeçalho.setAttribute("style", `background-color:rgb(${r}, ${g}, ${b})`)
+//     cabeçalho.setAttribute("style", `background-color:rgb(${r}, ${g}, ${b})`)
 
-    //Uma função  de callback, que executa algo e retorna algo: primeiro parâmetro é o que vai executar e o segundo, o tempo
-    tmp = setTimeout(mudaCor, 1000);
+//     //Uma função  de callback, que executa algo e retorna algo: primeiro parâmetro é o que vai executar e o segundo, o tempo
+//     tmp = setTimeout(mudaCor, 1000);
 
-}
+// }
 
-mudaCor();
+// mudaCor();
+//--------------------------------
+//EXERCÍCIO 1: MUDANDO E ROTACIONANDO A IMAGEM ENTRE OS 3 BANNERS
+//DICA DO EXERCÍCIO: A FUNÇÃO BANNER1 VAI PUXAR A FUNÇÃO BANNER2, QUE VAI PUXAR A FUNÇÃO BANNER3, POR FIM, PUXANDO A FUNÇÃO BANNER1
+
+// //função altera banner
+// function alteraBanner(){
+
+//     //o '>' serve para falar que dentre todos os elementos da div, queremos apenas aquele dentro do aside 
+//     const img1 = document.querySelector(".l-e > img");
+//     const img2 = document.querySelector(".l-d > img");
+
+//     let numero = Math.round(Math.random() * 3);
+//     let caminho = `./img/banner-lateral-${numero}.png`;
+//     img1.src = caminho;
+    
+//     let numero = Math.round(Math.random() * 3);
+//     let caminho = `./img/banner-lateral-${numero}.png`;
+//     img2.src = caminho;
+
+//     setTimeout(alteraBanner, 1000);
+
+// }
+
+// alteraBanner();
+
+//EXERCÍCIO 2: LIGAR E DESLIGAR A LÂMPADA
+
+// const botao = document.querySelector(".central button");
+// botao.addEventListener("click" , ligaDesliga);
+// botao.addEventListener("click" , ()=>{
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(botao.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     } else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// });
+
+// function ligaDesliga() {
+
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(botao.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     } else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// }
+
+//______________________________________________________________________________05/02/2023
+
+const inputUser = document.querySelector("input[name='nmUser']");
+const labelUser = document.querySelector("label[for]");
+
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style", "outline-color:#ff0000;");
+});
+
+inputUser.addEventListener("keyup", ()=>{
+    if(inputUser.value.length <= 8){
+        inputUser.setAttribute("style", "outline-color:#ff0000;");
+        labelUser.setAttribute("style", "color:#ff0000;");
+    } else{
+        inputUser.setAttribute("style", "outline-color:#dddddd;");
+        labelUser.setAttribute("style", "color:#000000;");
+    }
+    
+});
