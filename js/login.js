57441b9872bc7e2315f0ec05381d4b5ae87e384c
bdancs <rm551645@fiap.com.br>
2023-05-23 11:48:43 -0300

    autenticação com localstorage + token/redirect

--- js/login.js
+++ js/login.js
@@ -1,18 +1,46 @@
 //______________________________________________________________________________09/05/2023
 //GERANDO DOIS OBJETOS NO LOCALSTORAGE
-const usuario1 = {
-    nomeUsuario:"predo",
-    senhaUsuario:"12345"
-}
+// const usuario1 = {
+//     nomeUsuario:"predo",
+//     senhaUsuario:"12345"
+// }
 
-const usuario2 = {
-    nomeUsuario:"anzina",
-    senhaUsuario:"12345"
-}
+// const usuario2 = {
+//     nomeUsuario:"anzina",
+//     senhaUsuario:"12345"
+// }
 
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+// let listaDeUsuarios = [];
+// listaDeUsuarios.push(usuario1);
+// listaDeUsuarios.push(usuario2);
+
+//GERANDO UMA LISTA DE USUÁRIOS(23/05/2023)
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "Pedro Silva",
+        nomeUsuario : "99999",
+        senhaUsuario : "123456",
+        avatarUsuario : "https://www.catlicious.com.br/img/blog/4ffaface5622100f56e41a6d4f8e7631eff815af.jpeg?w=750&h=425&fit=crop"
+    },
+    {
+        nomeCompleto : "Ana Paula",
+        nomeUsuario : "88888",
+        senhaUsuario : "123456",
+        avatarUsuario : "https://www.catlicious.com.br/img/blog/4ffaface5622100f56e41a6d4f8e7631eff815af.jpeg?w=750&h=425&fit=crop"
+    },
+    {
+        nomeCompleto : "Henquique Rico",
+        nomeUsuario : "77777",
+        senhaUsuario : "123456",
+        avatarUsuario : "https://www.catlicious.com.br/img/blog/4ffaface5622100f56e41a6d4f8e7631eff815af.jpeg?w=750&h=425&fit=crop"
+    },
+    {
+        nomeCompleto : "Guilherme Hora",
+        nomeUsuario : "66666",
+        senhaUsuario : "123456",
+        avatarUsuario : "https://www.catlicious.com.br/img/blog/4ffaface5622100f56e41a6d4f8e7631eff815af.jpeg?w=750&h=425&fit=crop"
+    },
+]
 
 localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
@@ -45,7 +73,7 @@ addEventListener("click", (evento)=>{
         //COMENTEI ESSES DAQUI QUANDO CRIEI O GETITEM AQUI EMBAIXO
 
         //Recuperando os inputs
-        let inputUserValue = document.querySelector("#idUser").value;
+        let inputRMValue = document.querySelector("#idRM").value;
         let inputPassValue = document.querySelector("#idPass").value;
 
         const h1Titulo = document.querySelector("#titulo");
@@ -59,16 +87,21 @@ addEventListener("click", (evento)=>{
             lista.forEach((usuario)=> {
 
             //Simulando uma VALIDAÇÃO
-                if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
+                if(inputRMValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
                     userValidado = usuario;
                     throw "VALIDADO!";    
                 }
             });
 
+            //Caso não ocorra validação o throw é lançado para o catch com a string referente.
                 throw "NÃO VALIDADO!";
 
         }catch(msg){
             if(msg == "VALIDADO!"){
+
+                //Criando o objeto userValidado no LocalStorage
+                localStorage.setItem("user-validado",  JSON.stringify(userValidado));
+
                 h1Titulo.innerHTML = "<span><strong>Login validado com sucesso!</strong></span>";
                 h1Titulo.setAttribute("style","color:#00ff00;");
 
@@ -76,10 +109,13 @@ addEventListener("click", (evento)=>{
                 userValidado["token"] = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
 
                 //Setando um novo objeto no LocalStorage
-                localStorage.setItem("UserValidado", JSON.stringify(userValidado));
+                localStorage.setItem("user-token", JSON.stringify(token));
 
-                //Direcionando o usuário para a página de sucesso:
-                window.location.href = "../sucesso.html";
+                setTimeout(()=>{
+                    //Direcionando o usuário para a página de sucesso!
+                    window.location.href = "../sucesso.html";
+                }, 3000);
+
             }else{
                 h1Titulo.innerHTML = "<span><strong>Login ou senha inválidos!</strong></span>";
                 h1Titulo.setAttribute("style","color:#ff0000;");
@@ -92,30 +128,30 @@ addEventListener("click", (evento)=>{
 
 //O local-storage serve para guardar informações localmente, como se fosse um banco de dados; pode desligar e ligar denovo o computador que não vai perder as informações salvas
 
-try{
-    const userBemVindo = document.querySelector("#userWelcome");
-    let usuario = JSON.parse(localStorage.getItem("UserValidado"));
+// try{
+//     const userBemVindo = document.querySelector("#userWelcome");
+//     let usuario = JSON.parse(localStorage.getItem("UserValidado"));
 
-    if(usuario){
-        if(usuario.tokenp){
-            userBemVindo.innerHTML = usuario.nomeUsuario;
-        }
+//     if(usuario){
+//         if(usuario.tokenp){
+//             userBemVindo.innerHTML = usuario.nomeUsuario;
+//         }
     
-    }else{
-        window.location.href = "../erro.html";
-    }
+//     }else{
+//         window.location.href = "../erro.html";
+//     }
 
-    const botaoLogout = document.querySelector("#btnLogout");
-    botaoLogout.addEventListener("click", ()=>{
-        localStorage.removeItem("UserValidado")
-        window.location.href = "../login.html"
+//     const botaoLogout = document.querySelector("#btnLogout");
+//     botaoLogout.addEventListener("click", ()=>{
+//         localStorage.removeItem("UserValidado")
+//         window.location.href = "../login.html"
 
-    });
+//     });
 
-}catch(erro){
+// }catch(erro){
 
-    if(userBemVindo != null){
-        userBemVindo.innerHTML = JSON.parse(localStorage.getItem("UserValidado")).nomeUsuario;
-    }
+//     if(userBemVindo != null){
+//         userBemVindo.innerHTML = JSON.parse(localStorage.getItem("UserValidado")).nomeUsuario;
+//     }
 
-}
+// }
